7cf027d948eaf201a93573eaeeca6ae4878ec0cf
netogu <ernesto.gonzalez.u@gmail.com>
2023-02-01 00:12:20 -0800

    Checkpoint: MA782 driver rdy to test

--- src/magsensor.cpp
+++ src/magsensor.cpp
@@ -1,10 +1,37 @@
 #include "magsensor.h"
 #include <libopencm3/stm32/spi.h>
 
+#define READ_ANGLE				0b000
+#define CLEAR_ERR_FLG 		0b001
+#define READ_REG					0b010
+#define WRITE_REG					0b100
+#define STORE_ALL				 	0b101
+#define STORE_REG 		    0b111
+#define CMD_HEADER(CMD_BITS, ADDRESS) (CMD_BITS << 5 | ADDRESS)
+
+#define PI2 6.283185307	
 
 
 MagAngleSensor::MagAngleSensor(){
+	_spi = 0;
+	 
 }
+
+
+uint16_t MagAngleSensor::_send_cmd(uint8_t cmd_header, uint8_t reg_val){
+
+	uint16_t rx;
+
+	spi_enable(_spi);
+	spi_xfer(_spi, cmd_header);
+	spi_xfer(_spi, reg_val);
+	rx = spi_xfer(_spi, 0x00) << 8;
+	rx |= spi_xfer(_spi, 0x00);
+
+	return rx;
+
+}
+
 
 void MagAngleSensor::begin(uint32_t spi_base){
 	_spi = spi_base;
@@ -12,20 +39,83 @@ void MagAngleSensor::begin(uint32_t spi_base){
 
 
 uint16_t MagAngleSensor::read_angle_raw(){
-	uint16_t raw_angle = 0;
-	spi_enable(_spi);
-	raw_angle = spi_xfer(_spi, 0x00) << 8;
-	raw_angle |= spi_xfer(_spi, 0x00);
-	spi_disable(_spi);
-	return raw_angle;
+	uint16_t angle_raw = 0;
+	angle_raw = _send_cmd(CMD_HEADER(READ_ANGLE, 0x00), 0x00);
+	return angle_raw;
 
 }
 
-float MagAngleSensor::read_angle(){
+
+double MagAngleSensor::read_angle_deg(){
+	double angle;
 	uint16_t angle_raw = 0;
-	float angle_degree;
-	angle_raw = read_angle_raw();
-	angle_degree = (angle_raw*360.0f)/65536.0f;
-	return angle_degree;
+	angle_raw = _send_cmd(CMD_HEADER(READ_ANGLE, 0x00), 0x00);
+	angle = (angle_raw*360.0)/65536.0;
+
+	return angle;
 
 } 
+
+
+double MagAngleSensor::read_angle_rad(){
+	double angle;
+	uint16_t angle_raw = 0;
+	angle_raw = _send_cmd(CMD_HEADER(READ_ANGLE, 0x00), 0x00);
+	angle = (angle_raw*PI2)/65536.0;
+
+	return angle;
+
+} 
+
+
+uint16_t MagAngleSensor::read_register(uint16_t address){
+	uint16_t value;
+	value = _send_cmd(CMD_HEADER(READ_REG, address), 0x00);
+	return value;
+
+}
+
+
+bool MagAngleSensor::write_register(uint16_t address, uint16_t value){
+
+	uint16_t new_value;
+	new_value = _send_cmd(CMD_HEADER(WRITE_REG, address), value);
+
+	if(new_value == value)
+		return true;
+	else	
+		return false;
+
+}
+
+
+void MagAngleSensor::store_single_register(uint16_t address){
+
+	_send_cmd(CMD_HEADER(STORE_REG, address), 0x00);
+
+}
+
+
+void MagAngleSensor::store_all_registers(){
+
+	_send_cmd(CMD_HEADER(STORE_ALL, 0x00), 0x00);
+
+}
+
+
+void MagAngleSensor::clear_errors(){
+
+	_send_cmd(CMD_HEADER(CLEAR_ERR_FLG, 0x00), 0x00);
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- src/magsensor.h
+++ src/magsensor.h
@@ -6,24 +6,23 @@
 /* Based on MA782 */
 class MagAngleSensor 
 {
+	private:
+		uint32_t _spi;
+		inline uint16_t _send_cmd(uint8_t cmd_header, uint8_t reg_val);
 
 
 	public:
 
 		MagAngleSensor();
 		void begin(uint32_t spi_base);
-		float read_angle();
+		double read_angle_deg();
+		double read_angle_rad();
 		uint16_t read_angle_raw();
-		uint16_t read_register();
-		uint16_t write_register();
-		uint16_t store_single_register();
-		uint16_t store_all_registers();
-		uint16_t restore_all_registers();
-		uint16_t clear_errors();
-
-
-	private:
-		uint32_t _spi;
+		uint16_t read_register(uint16_t address);
+		bool write_register(uint16_t address, uint16_t value);
+		void store_single_register(uint16_t address);
+		void store_all_registers();
+		void clear_errors();
 
 
 };
